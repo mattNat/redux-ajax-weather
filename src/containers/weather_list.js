b455cfe07216650e66447d513d3238c20880735f
mattNat <mattsanpedro76@gmail.com>
2018-03-10 20:45:23 -0800

    add sparkline chart tempsF in /containers/weather_list.js

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 // data in from redux
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
+    const tempsF = cityData.list.map(weather => weather.main.temp*9/5 - 459.67);
+    const tempsK = cityData.list.map(weather => weather.main.temp);
+    
+    // console.log('In F:', tempsF, 'In K:', tempsK);
 
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          {/* pixel values */}
+          <Sparklines height={120} with={180} data={tempsF}>
+            {/* look at docs to see available colors */}
+            <SparklinesLine color='red' />
+          </Sparklines>
+        </td>
       </tr>
     )
   }
